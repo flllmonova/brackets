1d04dc43a0783463a6ad5e558e3cc50359ba59a8
Irina Filimonova <flllmonova@yandex.ru>
2026-05-02 16:09:23 +0500

    feat: solve brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-module.exports = function check(/* str, bracketsConfig */) {
-  throw new Error('Not implemented');
+module.exports = function check(str, bracketsConfig) {
+  const bracketsPairs = bracketsConfig.map((item) => item.join(''));
+  let result = str;
+
+  while (result.length > 0) {
+    const newStr = bracketsPairs.reduce((acc, brackets) => {
+      return acc.replace(brackets, '');
+    }, result);
+
+    if (newStr === result) {
+      return false;
+    }
+
+    result = newStr;
+  }
+  return true;
 };
